f6ba0eec6099186d754ed5d73f2044884566538a
Kevin Hsieh <2467001+crowbat@users.noreply.github.com>
2026-09-03 17:47:27 -0700

    Allow centroid recomputation when fake palettizers are switched from disabled to enabled (#86)

--- src/coreai_opt/palettization/kmeans/palettizer.py
+++ src/coreai_opt/palettization/kmeans/palettizer.py
@@ -394,7 +394,10 @@ class KMeansPalettizer(_BasePalettizer, _EagerCompressionComponentBuilderMixin):
 
     def _apply_schedule(self) -> None:
         for fp_module, schedule in self._fp_to_schedule.items():
-            fp_module.enable_fake_palett(schedule._compute_state(self._step_count))
+            new_state = schedule._compute_state(self._step_count)
+            # On the warm-up -> enabled transition, re-cluster from the current
+            # (warmed-up) weights instead of the frozen prepare-time centroids.
+            fp_module.enable_fake_palett(new_state, reinitialize=new_state)
 
     def _validate_mmap_dir_constraints(
         self,
